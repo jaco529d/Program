36794648d73a1a572af752713027bcf2b9d892b3
jaco529d <jaco529d@study.sde.dk>
2023-03-22 09:26:13 +0100

    Made NN-class and init in main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from neuralNetwork import *
+
+#parameters:
+inputNodes = 3
+hiddenNodes = 3
+outputNodes = 3
+learningRate = 0.3
+
+n = neuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
---
+++ neuralNetwork.py
@@ -0,0 +1,20 @@
+class neuralNetwork:
+
+    #initialise network
+    def __init__(self, inputNodes,hiddenNodes, outputNodes, learningRate):
+        #set numbers of nodes for each layer
+        self.inputNodes = inputNodes
+        self.hiddenNodes = hiddenNodes
+        self.outputNodes = outputNodes
+
+        #set learning rate 
+        self.learningRate = learningRate
+        pass
+
+    #train function
+    def train(self):
+        pass
+
+    #query/answer
+    def query(self):
+        pass
