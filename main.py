f77027d1eda70767ec710dcc68c19403f3bb625d
jaco529d <jaco529d@study.sde.dk>
2023-03-27 11:36:31 +0200

    end of hour, next train on pictures

--- main.py
+++ main.py
@@ -10,4 +10,11 @@ n = neuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
 
 a = n.query([1.0, 0.5, -1.5])
 print(a)
+
+#Open pictures
+
+#reshape til 32*32 billede (evt. biblioteket pillow) og tag pixelværdier og sæt i rækkefølge efter 
+#hinanden (numpy.reshape) således du får en streng adskilt af komma'er. Skal billedet croppes eller 
+#sætte black bars på?
+
 #n.debug()
