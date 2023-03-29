fbd170dc0fd9c5d3c7b0921fe016adc39ffd2bf2
jaco529d <jaco529d@study.sde.dk>
2023-03-29 13:47:05 +0200

    training added and test on 2 pics, not correct?

--- main.py
+++ main.py
@@ -2,17 +2,46 @@ from neuralNetwork import *
 from pic2list import *
 
 #parameters:
-inputNodes = 3
-hiddenNodes = 3
-outputNodes = 3
+inputNodes = 1024
+hiddenNodes = 1024
+outputNodes = 1
 learningRate = 0.3
 
 n = neuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
 
-a = n.query([1.0, 0.5, -1.5])
-print(a)
-
 #Open pictures
+trainList = []
+trainAnswerList = []
+
+for i in range(4000):
+    i += 1
+
+    img_path = f"C:/Users/jacob/OneDrive - Syddansk Erhvervsskole/Programering/Eksamensprojekt/dataset/training_set/cats/cat.{i}.jpg"
+    pixels = resize_image(img_path)
+    
+    trainList.append(pixels)
+    trainAnswerList.append(1)
+    print(i)
+
+for i in range(4000):
+    i += 1
+
+    img_path = f"C:/Users/jacob/OneDrive - Syddansk Erhvervsskole/Programering/Eksamensprojekt/dataset/training_set/dogs/dog.{i}.jpg"
+    pixels = resize_image(img_path)
+    
+    trainList.append(pixels)
+    trainAnswerList.append(0)
+    print(i)
+
+for i in range(len(trainList)):
+    n.train(trainList[i], trainAnswerList[i])
+
+catTest = n.query(resize_image('C:/Users/jacob/OneDrive - Syddansk Erhvervsskole/Programering/Eksamensprojekt/dataset/test_set/cats/cat.4001.jpg'))
+dogTest = n.query(resize_image('C:/Users/jacob/OneDrive - Syddansk Erhvervsskole/Programering/Eksamensprojekt/dataset/test_set/dogs/dog.4001.jpg'))
+
+print("cat: ", int(catTest))
+print("dog: ", int(dogTest))
+input('press enter to close')
 
 #reshape til 32*32 billede (evt. biblioteket pillow) og tag pixelværdier og sæt i rækkefølge efter 
 #hinanden (numpy.reshape) således du får en streng adskilt af komma'er. Skal billedet croppes eller 
